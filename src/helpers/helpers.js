141910469c5d80471dd974b4b5cfc2d387328fa5
Artush <mkrtchyanartush1989@gmail.com>
2024-10-28 10:35:16 +0400

    Update helpers.js
    hasDuplicateValueArrayOfObjectsBykey

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,1 +1,6 @@
 export const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function hasDuplicateValueArrayOfObjectsBykey(array, key) {
+  const arr = array.map(item => item[key]);
+  return new Set(arr).size !== arr.length;
+}
